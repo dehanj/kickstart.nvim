bf493fd90cb6a2f016c3ed5e8651a28cf0c36f6a
dehanj <jobson@tillitis.se>
2024-04-05 23:31:04 +0200

    Add chatgpt

---
+++ lua/custom/plugins/chatgpt.lua
@@ -0,0 +1,16 @@
+return {
+  'jackMort/ChatGPT.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'nvim-lua/plenary.nvim',
+    'folke/trouble.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  config = function()
+    require('chatgpt').setup {
+      -- api_key_cmd = 'hidden',
+      model = 'gpt-3.5-turbo', -- Specify the model to use
+    }
+  end,
+}
